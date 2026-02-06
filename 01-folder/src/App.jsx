fe3c6b5a7520541a6115ec187e6262b268fe8548
mukeshrajput01 <mukeshrajput805782@gmail.com>
2026-02-06 12:03:20 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+          <h1>hello how are you all</h1>
+          <h3>i am fine and  you are also well.</h3>
+      </div>
+      <div>
+          <h1>hey all you are there</h1>
+      </div>
+      <h4>kndfosd</h4>
+    </>
+  )
+}
+
+export default App
+
